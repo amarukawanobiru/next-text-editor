a751e919875001c3c20ab1f7b5222264188b5876
amarukawanobiru <k4tsu0502@gmail.com>
2025-06-02 22:58:05 +0900

    feat: 画像削除処理をする api を追加

---
+++ src/app/api/image/delete/route.ts
@@ -0,0 +1,28 @@
+import { unlink } from "node:fs/promises";
+import path from "node:path";
+import { type NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/db/prisma";
+
+export const POST = async (req: NextRequest) => {
+  const { fileName } = await req.json();
+
+  try {
+    await prisma.$transaction(async (tx) => {
+      await tx.uploadedFile.delete({
+        where: { fileName },
+      });
+
+      const filePath = path.join(process.cwd(), "public/uploads", fileName);
+      await unlink(filePath);
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    } else {
+      console.error(error);
+    }
+    return NextResponse.json({ success: false });
+  }
+
+  return NextResponse.json({ success: true });
+};
